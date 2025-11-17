e4d7f38f8f0b9fc7db02f11e1924baaa67740d26
lagovini <lagovini@units.it>
2025-11-17 15:03:48 +0000

    miotema

--- drupal/web/themes/custom/miotema/js/script.js
+++ drupal/web/themes/custom/miotema/js/script.js
@@ -2,12 +2,13 @@
  * API JS Drupal: https://www.drupal.org/docs/drupal-apis/javascript-api/javascript-api-overview
  */
 console.log('No JS qua');
+
 (function (Drupal) {
-Drupal.behaviors.ciao = {
-  attach: function (context, settings) {
-    once('ciao', 'body').forEach(function (element) {
-      console.log('Si JS in funzione anonima', element);
-    })
+  Drupal.behaviors.ciao = {
+    attach: function (context, settings) {
+      once('ciao', 'body').forEach(function (element) {
+        console.log('Si JS qua in funzione anonima', element);
+      })
+    }
   }
-}
 })(Drupal);

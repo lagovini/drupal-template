c5532eff76e1c7f7f3c8bb2ca5767be35f18de20
lagovini <lagovini@units.it>
2025-11-18 09:10:06 +0000

    template nel blocco

--- drupal/web/modules/custom/miomodulo/src/Plugin/Block/MioBlocco.php
+++ drupal/web/modules/custom/miomodulo/src/Plugin/Block/MioBlocco.php
@@ -1,27 +1,48 @@
 <?php
-
 namespace Drupal\miomodulo\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 
 /**
- * Mio blocco custom.
+ * Mio blocco custom
  *
  * @Block(
  *   id = "mio_blocco_custom",
  *   admin_label = @Translation("Mio blocco custom"),
- *   category = @Translation("Corso Drupal"),
+ *   category = @Translation("Corso Drupal")
  * )
  */
 
-class MioBlocco extends BlockBase {
-    /**
-     * 
-     */
-    public function build() {
-        $now = \Drupal::service('date.formatter')->format(time(),'custom', 'd/m/Y H:i'); #servizio Drupal
-        return [
-            '#markup' => "<h2>mio blocco custom adesso sono $now</h2>",
-        ];
-    }
-}
+ class MioBlocco extends BlockBase {
+
+  public function build () {
+    $data = \Drupal::service('date.formatter')->format(time(), 'custom', 'd/m/Y H:i');
+
+    $payload = [
+      'title' => 'Lista insegnamenti',
+      'items' => [
+        [
+          'des' => 'Insegnamento 1',
+          'peso' => '6'
+        ],
+        [
+          'des' => 'Insegnamento 2',
+          'peso' => '7'
+        ],
+        [
+          'des' => 'Insegnamento 3',
+          'peso' => '9'
+        ],
+      ],
+    ];
+
+    dump($payload);
+
+    return [
+      '#theme' => 'miomodulo_mioblocco',
+      '#now' => $data,
+      '#payload' => $payload,
+    ];
+  }
+
+ }
